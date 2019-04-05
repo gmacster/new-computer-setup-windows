fff0da075f632a67a4ea5a5543df451c2babee03
Graham MacMaster <graham.macmaster@gmail.com>
2019-04-04 17:35:38 -0700

    Add missing function for setting registry values

--- Tweaks/RemoveAds.ps1
+++ Tweaks/RemoveAds.ps1
@@ -1,5 +1,12 @@
 Write-Output "Removing ads from the start menu, task bar, and lock screen..."
 
+function Set-Registry([string]$Path, [string]$Name, [string]$PropertyType, $Value) {
+    if (!(Test-Path $Path)) {
+        New-Item -Path $Path -Force | Out-Null
+    }
+    New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType $PropertyType -Force | Out-Null
+}
+
 $cdmRegistry = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
 Set-Registry -Path $cdmRegistry -Name SubscribedContent-338388Enabled -Value 0
 Set-Registry -Path $cdmRegistry -Name SubscribedContent-338389Enabled -Value 0
